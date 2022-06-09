24bc8d98b6dcad165b6ee26be2a046d4c065a6ed
Scheph <omarscheph18@gmail.com>
2022-06-09 20:39:32 +0000

    CRUD features implementation

--- src/main/java/com/spring/api/school/rest/controller/ApiControllers.java
+++ src/main/java/com/spring/api/school/rest/controller/ApiControllers.java
@@ -5,14 +5,18 @@
 
 package com.spring.api.school.rest.controller;
 
+import com.spring.api.school.rest.models.Localite;
 import com.spring.api.school.rest.models.Pays;
 import com.spring.api.school.rest.repository.LocaliteRepository;
 import com.spring.api.school.rest.repository.PaysRepository;
 import com.spring.api.school.rest.services.GestLocaliteInterface;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,7 +36,8 @@ public class ApiControllers {
         return "Welcome";
     }
     
-    @PostMapping(value = "/save")
+    //Pays CRUD
+    @PostMapping(value = "/save_pays")
     public String createPays(@RequestBody Pays pays) {
         return gestLocImpl.createPays(pays);
     }
@@ -40,6 +45,38 @@ public class ApiControllers {
     @GetMapping(value = "/pays")
     public List<Pays> readPays(){
         return gestLocImpl.readPays();
+    }
+    
+    @PutMapping(value = "/update_localite/{id}")
+    public String updatePays(@PathVariable long id, @RequestBody Pays pays){
+        return gestLocImpl.updatePays(id, pays);
+    }
+    
+    @DeleteMapping(value = "/delete_pays")
+    public String deletePays(@RequestBody Pays pays){
+        return gestLocImpl.deletePays(pays);
+    }
+    
+    
+    //Localite CRUD
+    @PostMapping(value = "/save_localite")
+    public String createLocalite(@RequestBody Localite localite){
+        return gestLocImpl.createLocalite(localite);
+    }
+    
+    @GetMapping(value = "/localites")
+    public List<Localite> readLocalite(){
+        return gestLocImpl.readLocalite();
+    }
+    
+    @PutMapping(value = "/update_localite/{id}")
+    public String updateLocalite(@PathVariable long id, @RequestBody Localite localite){
+        return gestLocImpl.updateLocalite(id, localite);
+    }
+    
+    @DeleteMapping(value = "/delete_localite")
+    public String deleteLocalite(@RequestBody Localite loclite){
+        return gestLocImpl.deleteLocalite(loclite);
     }
 
 }
